a950308973bbdc4a84bbb49ac2ea596af341f05b
Sultani Sanan <54854030+sultani93@users.noreply.github.com>
2022-11-02 18:41:13 +0400

    router structure and header footer done

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import Routes from '../../routes/Routers'
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <Routes />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/Home.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Helmet from '../components/Helmet/Helmet'
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap'
+import heroImg from '../assets/images/hero.png'
+import '../styles/hero-section.css'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <Helmet title='Home'>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' md='6'>
+              <div className='hero__content'>
+                <h5 className='mb-3'>Easy way to make an order </h5>
+                <h1 className='mb-4 hero__title'>
+                  <span>HUNGRY?</span> Just wait <br />
+                  food at <span> your door</span>
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Exercitationem autem est repellendus iusto hic maiores!
+                </p>
+                <div className='hero__btns d-flex align-items-center gap-5 mt-4'>
+                  <button className='order__btn d-flex align-items-center justify-content-between'>
+                    Order now
+                    <i className='ri-arrow-right-s-line'></i>
+                  </button>
+                  <button className='all__foods-btn'>
+                    <Link to='/foods'>See all foods</Link>
+                  </button>
+                </div>
+                <div>
+                  <p>
+                    <span className='shipping__icon'>
+                      <i class='ri-car-line'></i> No shipping charge
+                    </span>
+                  </p>
+                  <p>
+                    <span className='shipping__icon'>
+                      <i class='ri-shield-check-line'></i> 100% secure checkout
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col lg='6' md='6'>
+              <div className='hero__img'>
+                <img src={heroImg} alt='delivery' className='w-100' />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  )
+}
+
+export default Home
