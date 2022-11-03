cc93d4357c644f50f19dc0c8947670cc9d1f7c03
Sultani Sanan <54854030+sultani93@users.noreply.github.com>
2022-11-03 18:26:10 +0400

    home tasty

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import Helmet from '../components/Helmet/Helmet'
 import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap'
 import heroImg from '../assets/images/hero.png'
 import '../styles/hero-section.css'
 import { Link } from 'react-router-dom'
+import Category from '../components/UI/category/Category'
+import '../styles/home.css'
+import featureImg01 from '../assets/images/service-01.png'
+import featureImg02 from '../assets/images/service-02.png'
+import featureImg03 from '../assets/images/service-03.png'
+import products from '../assets/fake-data/products'
+import foodCategoryImg01 from '../assets/images/hamburger.png'
+import foodCategoryImg02 from '../assets/images/pizza.png'
+import foodCategoryImg03 from '../assets/images/bread.png'
+import ProductCard from '../components/UI/product-card/ProductCard'
+import whyImg from '../assets/images/location.png'
+
+const featureData = [
+  {
+    title: 'Quick Delivery',
+    imgUrl: featureImg01,
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, unde.',
+  },
+  {
+    title: 'Super Dine In',
+    imgUrl: featureImg02,
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, unde.',
+  },
+  {
+    title: 'Easy Pick Up',
+    imgUrl: featureImg03,
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, unde.',
+  },
+]
 
 const Home = () => {
+  const [category, setCategory] = useState('ALL')
+  const [allproducts, setAllProducts] = useState(products)
+
+  useEffect(() => {
+    if (category === 'ALL') {
+      setAllProducts(products)
+    }
+    if (category === 'BURGER') {
+      const filteredProducts = products.filter(
+        (item) => item.category === 'Burger'
+      )
+      setAllProducts(filteredProducts)
+    }
+
+    if (category === 'PIZZA') {
+      const filteredProducts = products.filter(
+        (item) => item.category === 'Pizza'
+      )
+      setAllProducts(filteredProducts)
+    }
+
+    if (category === 'BREAD') {
+      const filteredProducts = products.filter(
+        (item) => item.category === 'Bread'
+      )
+      setAllProducts(filteredProducts)
+    }
+  }, [category])
+
   return (
     <Helmet title='Home'>
       <section>
@@ -31,16 +89,18 @@ const Home = () => {
                     <Link to='/foods'>See all foods</Link>
                   </button>
                 </div>
-                <div>
-                  <p>
-                    <span className='shipping__icon'>
-                      <i class='ri-car-line'></i> No shipping charge
+                <div className='hero__service d-flex align-items-center gap-5 mt-5'>
+                  <p className='d-flex align-items-center gap-2'>
+                    <span className='shipping__icon '>
+                      <i class='ri-car-line'></i>
                     </span>
+                    No shipping charge
                   </p>
-                  <p>
+                  <p className='d-flex align-items-center gap-2'>
                     <span className='shipping__icon'>
-                      <i class='ri-shield-check-line'></i> 100% secure checkout
+                      <i class='ri-shield-check-line'></i>
                     </span>
+                    100% secure checkout
                   </p>
                 </div>
               </div>
@@ -48,6 +108,153 @@ const Home = () => {
             <Col lg='6' md='6'>
               <div className='hero__img'>
                 <img src={heroImg} alt='delivery' className='w-100' />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className='pt-0'>
+        <Category />
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12' className='text-center'>
+              <h5 className='feature__subtitle'>What we serve</h5>
+              <h2 className='feature__title'>Just sit back at home</h2>
+              <h2 className='feature__title'>
+                We will <span>take care</span>
+              </h2>
+              <p className='mb-1 mt-4 feature__text'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Debitis ipsam sed mollitia dolorum earum quia.
+              </p>
+              <p className='feature__text'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+                accusamus?
+              </p>
+            </Col>
+            {featureData.map((item, index) => (
+              <Col lg='4' md='4' key={index} className='mt-5'>
+                <div className='feature__item text-center px-5 py-3'>
+                  <img
+                    src={item.imgUrl}
+                    alt='feature-img '
+                    className='w-25 mb-3'
+                  />
+                  <h5 className='fw-bold mb-3'>{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12' className='text-center'>
+              <h2>Popular Foods</h2>
+            </Col>
+
+            <Col lg='12'>
+              <div className='food__category d-flex align-items-center justify-content-center gap-4'>
+                <button
+                  className={`all__btn ${
+                    category === 'ALL' ? 'foodBtnActive ' : ' '
+                  }`}
+                  onClick={() => setCategory('ALL')}
+                >
+                  All
+                </button>
+                <button
+                  className={`'d-flex align-items-center gap-2' ${
+                    category === 'BURGER' ? 'foodBtnActive' : ''
+                  }`}
+                  onClick={() => setCategory('BURGER')}
+                >
+                  <img src={foodCategoryImg01} alt='' />
+                  Burger
+                </button>
+                <button
+                  className={`'d-flex align-items-center gap-2' ${
+                    category === 'PIZZA' ? 'foodBtnActive' : ''
+                  }`}
+                  onClick={() => setCategory('PIZZA')}
+                >
+                  <img src={foodCategoryImg02} alt='' />
+                  Pizza
+                </button>
+                <button
+                  className={`'d-flex align-items-center gap-2' ${
+                    category === 'BREAD' ? 'foodBtnActive' : ''
+                  }`}
+                  onClick={() => setCategory('BREAD')}
+                >
+                  <img src={foodCategoryImg03} alt='' />
+                  Bread
+                </button>
+              </div>
+            </Col>
+            {allproducts.map((item) => (
+              <Col lg='3' md='4' key={item.id} className='mt-5'>
+                <ProductCard item={item} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' md='6'>
+              <img src={whyImg} alt='why-tasty-treat' className='w-100' />
+            </Col>
+            <Col lg='6' md='6'>
+              <div className='why__tasty-treat'>
+                <h2 className='tasty__treat-title mb-4'>
+                  Why <span> Tasty Treat?</span>
+                </h2>
+                <p className='tasty__treat-desc'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laborum est maiores molestiae adipisci! Error repellat
+                  possimus cupiditate quos assumenda? Hic dicta officiis quo
+                  sequi blanditiis numquam facilis magnam nisi quos.
+                </p>
+                <ListGroup className='mt-5'>
+                  <ListGroupItem className='border-0 ps-0'>
+                    <p className='d-flex align-items-center gap-2 choose__us-title'>
+                      <i className='ri-checkbox-circle-line'></i> Fresh and
+                      tasty Foods
+                    </p>
+                    <p className='choose__us-desc'>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Quia, ipsam.
+                    </p>
+                  </ListGroupItem>
+                  <ListGroupItem className='border-0 ps-0'>
+                    <p className='d-flex align-items-center gap-2 choose__us-title'>
+                      <i className='ri-checkbox-circle-line'></i>Quality support
+                    </p>
+                    <p className='choose__us-desc'>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Accusamus, amet!
+                    </p>
+                  </ListGroupItem>
+                  <ListGroupItem className='border-0 ps-0'>
+                    <p className='d-flex align-items-center gap-2 choose__us-title'>
+                      <i className='ri-checkbox-circle-line'></i>Order from any
+                      location
+                    </p>
+                    <p className='choose__us-desc'>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ad, dolor!
+                    </p>
+                  </ListGroupItem>
+                </ListGroup>
               </div>
             </Col>
           </Row>
